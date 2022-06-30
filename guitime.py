7818b8b914dcc4c447fa9777b9710fd4aaa0bc21
digstm0126 <78323032+digstm0126@users.noreply.github.com>
2022-06-30 15:01:05 +1200

    added the start of gui code

---
+++ guitime.py
@@ -0,0 +1,16 @@
+''' into to tkinter '''
+
+# inports tkinter
+from tkinter import *
+
+# set up the root windo
+root = Tk()
+root.title("my first tkinter gui very cool")
+root.resizable(width=FALSE, height=FALSE)
+root.geometry("800x500")
+
+# add a label
+heading_lbl = Label(root, text="brak the bad", fg="blue", font=("papyrus, 25"))
+heading
+
+root.mainloop()
